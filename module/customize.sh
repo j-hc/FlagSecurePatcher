7c6e067b33488041742c891fe218f1a3a53898d9
j-hc <j-hc@users.noreply.github.com>
2024-05-23 03:54:40 +0300

    lets not forget the optimizations

--- module/customize.sh
+++ module/customize.sh
@@ -33,9 +33,7 @@ BEGIN {
     in_method = 1
     print
     print "    .registers 1"
-    print "    "
     print "    const/4 v0, 0x0"
-    print "    "
     print "    return v0"
     next
 }
@@ -52,13 +50,13 @@ mv -f "$TMP_TARGET_CLASS" "$TARGET_SMALI"
 
 if [ "$API" -ge 34 ]; then
     ui_print "* Patching notifyScreenshotListeners (SDK level >= 34)"
-TARGET=$(grep -rn -Fx '.method public notifyScreenshotListeners(I)Ljava/util/List;' "$TMPPATH/services-da")
-[ -z "$TARGET" ] && abort "Method not found"
-TARGET_SMALI="${TARGET%%:*}"
-TARGET_CLASS="${TARGET_SMALI#"$TMPPATH/services-da/"}"
-TARGET_CLASS="${TARGET_CLASS%%/*}"
-TMP_TARGET_CLASS="$TMPPATH/${TARGET_SMALI##*/}"
-awk '
+    TARGET=$(grep -rn -Fx '.method public notifyScreenshotListeners(I)Ljava/util/List;' "$TMPPATH/services-da")
+    [ -z "$TARGET" ] && abort "Method not found"
+    TARGET_SMALI="${TARGET%%:*}"
+    TARGET_CLASS="${TARGET_SMALI#"$TMPPATH/services-da/"}"
+    TARGET_CLASS="${TARGET_CLASS%%/*}"
+    TMP_TARGET_CLASS="$TMPPATH/${TARGET_SMALI##*/}"
+    awk '
 BEGIN {
     in_method = 0
 }
@@ -74,11 +72,8 @@ BEGIN {
     print "            \">;\""
     print "        }"
     print "    .end annotation"
-    print "    "
     print "    invoke-static {}, Ljava/util/Collections;->emptyList()Ljava/util/List;"
-    print "    "
     print "    move-result-object p1"
-	print "    "
     print "    return-object p1"
     next
 }
@@ -91,7 +86,7 @@ BEGIN {
     if (!in_method) print
 }
 ' "$TARGET_SMALI" >"$TMP_TARGET_CLASS"
-mv -f "$TMP_TARGET_CLASS" "$TARGET_SMALI"
+    mv -f "$TMP_TARGET_CLASS" "$TARGET_SMALI"
 fi
 
 ui_print "* Re-assembling"
@@ -110,7 +105,16 @@ LD_LIBRARY_PATH=$LIBPATH zipalign -p -z 4 "$TMPPATH/services-patched.zip" "$MODP
 set_perm "$MODPATH/system/framework/services.jar" 0 0 644 u:object_r:system_file:s0
 
 ui_print "* Cleanup"
+if [ "$ARCH" = x64 ]; then INS_SET=x86_64; else INS_SET=$ARCH; fi
 rm -r "$TMPPATH" "$MODPATH/util"
+rm "/data/dalvik-cache/$INS_SET/system@framework@services.jar@classes.dex" || :
+
+ui_print "* Optimizing"
+mkdir "$MODPATH/system/framework/oat/$INS_SET"
+dex2oat --dex-file="$MODPATH/system/framework/services.jar" --instruction-set="$INS_SET" \
+    --oat-file="$MODPATH/system/framework/oat/$INS_SET/services.odex"
+set_perm "$MODPATH/system/framework/oat/$INS_SET/services.odex" 0 0 644 u:object_r:system_file:s0
+set_perm "$MODPATH/system/framework/oat/$INS_SET/services.vdex" 0 0 644 u:object_r:system_file:s0
 
 ui_print ""
 ui_print "  by github.com/j-hc"
