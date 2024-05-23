1330a68a3e545108b16f68df877cc40d0cfb3176
j-hc <j-hc@users.noreply.github.com>
2024-05-23 13:32:47 +0300

    more dex2oat flags

--- module/customize.sh
+++ module/customize.sh
@@ -4,18 +4,20 @@ set -e
 
 LIBPATH="$MODPATH/util/lib/${ARCH}"
 export PATH="${PATH}:$MODPATH/util/bin/${ARCH}"
-TMPPATH="$MODPATH/tmp"
 chmod -R 755 "$MODPATH/util/"
+
+TMPPATH="$MODPATH/tmp"
 mkdir "$TMPPATH"
 
-cp /system/framework/services.jar "$TMPPATH"
+cp "$(magisk --path)/.magisk/mirror/system/framework/services.jar" "$TMPPATH" \
+    || cp /system/framework/services.jar "$TMPPATH"
 
 ui_print "* Extracting services"
 unzip -q "$TMPPATH/services.jar" -d "$TMPPATH/services"
 for C in "$TMPPATH"/services/classes*; do
     if [ "$C" = "$TMPPATH/services/classes*" ]; then
-        ls -l "$TMPPATH/services/"
-        abort "classes glob fail"
+        ui_print "classes glob fail"
+        abort "ROM is not supported"
     fi
     O="${C##*/}"
     O="${O%.*}"
@@ -28,7 +30,6 @@ patch() {
     signature="$1"
     code="$2"
     TARGET=$(grep -rn -x "$signature" "$TMPPATH/services-da") || abort "Method not found"
-    [ -z "$TARGET" ] && abort "Method not found"
     TARGET_NR="${TARGET%:*}"
     TARGET_NR="${TARGET_NR##*:}"
     TARGET_SMALI="${TARGET%%:*}"
@@ -37,11 +38,10 @@ patch() {
     TMP_TARGET_CLASS="$TMPPATH/${TARGET_SMALI##*/}"
 
     awk -v TARGET_NR="$TARGET_NR" -v CODE="$code" '
-BEGIN { split(CODE, lines, "\n") }
 NR == TARGET_NR {
     in_method = 1
     print
-    for (i in lines) print lines[i]
+    print CODE
     next
 }
 /\.end method/ && in_method {
@@ -63,7 +63,6 @@ patch '\.method .*isSecureLocked(.*)Z' "$isSecureLockedCode"
 
 if [ "$API" -ge 34 ]; then
     ui_print "* Patching notifyScreenshotListeners (SDK level >= 34)"
-
     notifyScreenshotListenersCode='
 .registers 2
 .annotation system Ldalvik/annotation/Signature;
@@ -95,16 +94,22 @@ LD_LIBRARY_PATH=$LIBPATH zipalign -p -z 4 "$TMPPATH/services-patched.zip" "$MODP
 set_perm "$MODPATH/system/framework/services.jar" 0 0 644 u:object_r:system_file:s0
 
 ui_print "* Cleanup"
-if [ "$ARCH" = x64 ]; then INS_SET=x86_64; else INS_SET=$ARCH; fi
 rm -r "$TMPPATH" "$MODPATH/util"
-rm "/data/dalvik-cache/$INS_SET/system@framework@services.jar@classes.dex" 2>/dev/null || :
+if [ "$ARCH" = x64 ]; then INS_SET=x86_64; else INS_SET=$ARCH; fi
+rm "/data/dalvik-cache/$INS_SET/system@framework@services.jar@classes.dex" || :
+rm "/data/dalvik-cache/$INS_SET/system@framework@services.jar@classes.vdex" || :
 
 ui_print "* Optimizing"
 mkdir "$MODPATH/system/framework/oat/$INS_SET"
-dex2oat --dex-file="$MODPATH/system/framework/services.jar" --instruction-set="$INS_SET" \
-    --oat-file="$MODPATH/system/framework/oat/$INS_SET/services.odex"
-set_perm "$MODPATH/system/framework/oat/$INS_SET/services.odex" 0 0 644 u:object_r:system_file:s0
-set_perm "$MODPATH/system/framework/oat/$INS_SET/services.vdex" 0 0 644 u:object_r:system_file:s0
+dex2oat --dex-file="$MODPATH/system/framework/services.jar" --instruction-set="$INS_SET" --compiler-filter=everything \
+    --profile-file="/system/framework/services.jar.prof" --oat-file="$MODPATH/system/framework/oat/$INS_SET/services.odex" \
+    --app-image-file="$MODPATH/system/framework/oat/$INS_SET/services.art" || {
+    logcat -d -s "dex2oat" >/data/local/tmp/FlagSecurePatcher-dex2oat.log
+    abort "* Optimization step failed. Logs are saved to /data/local/tmp/FlagSecurePatcher-dex2oat.log"
+}
+for ext in odex vdex art; do
+    set_perm "$MODPATH/system/framework/oat/$INS_SET/services.${ext}" 0 0 644 u:object_r:system_file:s0
+done
 
 ui_print ""
 ui_print "  by github.com/j-hc"
