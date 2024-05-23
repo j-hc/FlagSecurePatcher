7e4e0fd9c96fc7e7f240a9fd91b290a7ee6cb1f6
j-hc <j-hc@users.noreply.github.com>
2024-05-23 05:57:05 +0300

    update method matching

--- module/customize.sh
+++ module/customize.sh
@@ -24,81 +24,65 @@ for C in "$TMPPATH"/services/classes*; do
         com.android.tools.smali.baksmali.Main d "$C" -o "$TMPPATH/services-da/$O"
 done
 
-ui_print "* Patching isSecureLocked"
-TARGET=$(grep -rn -x '.method .*isSecureLocked()Z' "$TMPPATH/services-da")
-[ -z "$TARGET" ] && abort "Method not found"
-TARGET_SMALI="${TARGET%%:*}"
-TARGET_CLASS="${TARGET_SMALI#"$TMPPATH/services-da/"}"
-TARGET_CLASS="${TARGET_CLASS%%/*}"
-TMP_TARGET_CLASS="$TMPPATH/${TARGET_SMALI##*/}"
-awk '
-BEGIN {
-    in_method = 0
-}
-/\.method .*isSecureLocked\(\)Z/ {
-    in_method = 1
-    print
-    print "    .registers 1"
-    print "    const/4 v0, 0x0"
-    print "    return v0"
-    next
-}
-/\.end method/ && in_method {
-    in_method = 0
-    print
-    next
-}
-{
-    if (!in_method) print
-}
-' "$TARGET_SMALI" >"$TMP_TARGET_CLASS"
-mv -f "$TMP_TARGET_CLASS" "$TARGET_SMALI"
-
-if [ "$API" -ge 34 ]; then
-    ui_print "* Patching notifyScreenshotListeners (SDK level >= 34)"
-    TARGET=$(grep -rn -x '.method .*notifyScreenshotListeners(I)Ljava/util/List;' "$TMPPATH/services-da")
+patch() {
+    signature="$1"
+    code="$2"
+    TARGET=$(grep -rn -x "$signature" "$TMPPATH/services-da") || abort "Method not found"
     [ -z "$TARGET" ] && abort "Method not found"
+    TARGET_NR="${TARGET%:*}"
+    TARGET_NR="${TARGET_NR##*:}"
     TARGET_SMALI="${TARGET%%:*}"
     TARGET_CLASS="${TARGET_SMALI#"$TMPPATH/services-da/"}"
     TARGET_CLASS="${TARGET_CLASS%%/*}"
     TMP_TARGET_CLASS="$TMPPATH/${TARGET_SMALI##*/}"
-    awk '
-BEGIN {
-    in_method = 0
-}
-/\.method .*notifyScreenshotListeners\(I\)Ljava\/util\/List;/ {
+
+    awk -v TARGET_NR="$TARGET_NR" -v CODE="$code" '
+BEGIN { split(CODE, lines, "\n") }
+NR == TARGET_NR {
     in_method = 1
     print
-    print "    .registers 2"
-    print "    .annotation system Ldalvik/annotation/Signature;"
-    print "        value = {"
-    print "            \"(I)\","
-    print "            \"Ljava/util/List<\","
-    print "            \"Landroid/content/ComponentName;\","
-    print "            \">;\""
-    print "        }"
-    print "    .end annotation"
-    print "    invoke-static {}, Ljava/util/Collections;->emptyList()Ljava/util/List;"
-    print "    move-result-object p1"
-    print "    return-object p1"
+    for (i in lines) print lines[i]
     next
 }
 /\.end method/ && in_method {
-    in_method = 0
     print
+    in_method = 0
     next
 }
-{
-    if (!in_method) print
-}
+{ if (!in_method) print }
 ' "$TARGET_SMALI" >"$TMP_TARGET_CLASS"
     mv -f "$TMP_TARGET_CLASS" "$TARGET_SMALI"
+}
+
+ui_print "* Patching isSecureLocked"
+isSecureLockedCode='
+.registers 1
+const/4 v0, 0x0
+return v0'
+patch '\.method .*isSecureLocked(.*)Z' "$isSecureLockedCode"
+
+if [ "$API" -ge 34 ]; then
+    ui_print "* Patching notifyScreenshotListeners (SDK level >= 34)"
+
+    notifyScreenshotListenersCode='
+.registers 2
+.annotation system Ldalvik/annotation/Signature;
+    value = {
+        "(I)",
+        "Ljava/util/List<",
+        "Landroid/content/ComponentName;",
+        ">;"
+    }
+.end annotation
+invoke-static {}, Ljava/util/Collections;->emptyList()Ljava/util/List;
+move-result-object p1
+return-object p1'
+    patch '\.method .*notifyScreenshotListeners(I)Ljava/util/List;' "$notifyScreenshotListenersCode"
 fi
 
 ui_print "* Re-assembling"
-A=$(getprop ro.build.version.sdk)
 ANDROID_DATA="$TMPPATH" CLASSPATH="$MODPATH/util/smali.jar" app_process "$MODPATH" \
-    com.android.tools.smali.smali.Main a -a "$A" "$TMPPATH/services-da/$TARGET_CLASS" \
+    com.android.tools.smali.smali.Main a -a "$API" "$TMPPATH/services-da/$TARGET_CLASS" \
     -o "$TMPPATH/services/$TARGET_CLASS.dex"
 
 ui_print "* Zipping"
