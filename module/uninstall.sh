a8f1764d4b9a4a41943ad737fc78d7a1e32d98b3
j-hc <j-hc@users.noreply.github.com>
2024-05-24 19:27:39 +0300

    many bugfixes and oneui support

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -2,3 +2,5 @@
 
 rm /data/dalvik-cache/*/system@framework@services.jar@classes.dex
 rm /data/dalvik-cache/*/system@framework@services.jar@classes.vdex
+rm /data/dalvik-cache/*/system@framework@semwifi-service.jar@classes.dex
+rm /data/dalvik-cache/*/system@framework@semwifi-service.jar@classes.vdex
