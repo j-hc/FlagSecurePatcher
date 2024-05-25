5d30194842c1889ebd8c8f84df5dcf31b9b2fc10
j-hc <j-hc@users.noreply.github.com>
2024-05-25 18:15:19 +0300

    clear more odex residue

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,6 +1,7 @@
 #!/system/bin/sh
 
-rm /data/dalvik-cache/*/system@framework@services.jar@classes.dex
-rm /data/dalvik-cache/*/system@framework@services.jar@classes.vdex
-rm /data/dalvik-cache/*/system@framework@semwifi-service.jar@classes.dex
-rm /data/dalvik-cache/*/system@framework@semwifi-service.jar@classes.vdex
+rm /data/dalvik-cache/*/system@framework@services.jar@classes.*
+rm /data/misc/apexdata/com.android.art/dalvik-cache/*/system@framework@services.jar@classes.*
+
+rm /data/dalvik-cache/*/system@framework@semwifi-service.jar@classes.*
+rm /data/misc/apexdata/com.android.art/dalvik-cache/*/system@framework@semwifi-service.jar@classes.*
